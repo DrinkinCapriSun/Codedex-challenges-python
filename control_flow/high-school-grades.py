fbf96df5eb62e721571e4ccdbc4877250f43cee4
Shi Wei <108122521+DrinkinCapriSun@users.noreply.github.com>
2023-06-25 03:03:54 +0200

    Add files via upload

---
+++ control_flow/high-school-grades.py
@@ -0,0 +1,13 @@
+grade = int(input('What grade are you? '))
+
+#INDENTATION SHOULD BE CONSISTENT SO IT'S NOT CONFUSING | I ALREADY FIXED IT.
+if grade == 9:
+  print('Freshman')
+elif grade == 10:
+  print('Sophomore')
+elif grade == 11:
+  print('Junior')
+elif grade == 12:
+  print('Senior')
+else:
+  print('TBD')
---
+++ control_flow/snapple-facts.py
@@ -0,0 +1,22 @@
+#MY ANSWER
+import random
+
+random = random.randint(0,5)
+
+if random == 0:
+    print('Flamingos turn pink from eating shrimp.')
+elif random == 1:
+    print('The only food that doesn’t spoil is honey.')
+elif random == 2:
+    print('Shrimp can only swim backwards.')
+elif random == 3:
+    print('The lifespan of a taste bud is about 10 days.')
+elif random == 4:
+    print('It is impossible to sneeze while sleeping.')
+else:
+    print('It is illegal to sing off-key in North Carolina.')
+
+#CODEDEX'S ANSWER
+#BEST ANSWER POSSIBLE!!!! JUST CHANGE THE NAME OF RANDOM VARIABLE TO SOMETHING ELSE SO IT'S NOT CONFUSING.
+
+
